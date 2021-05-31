4b2e4fd1bed465a3c25c14a97e7e99ca02c7ffce
taka <ookuma.taka.engineer@gmail.com>
2021-06-01 08:47:59 +0900

    second

--- practice_Jquery.js
+++ practice_Jquery.js
@@ -15,3 +15,28 @@ $(function(){
         }
     })
 })
+
+$(function(){
+    $(document).on('change','#ex3-select',function(){
+        if ($("#ex3-select").val() == "トライアスロン") {
+            $('#ex3-div').html('それでは好きな種目は？<input type="radio" name="subject">Swim <input type="radio" name="subject">Bike <input type="radio" name="subject">Run')
+        }
+        if ($("#ex3-select").val() == "その他") {
+            $('#ex3-div').html('それでは何が好き？<input type="text" name="subject">')
+        }
+    })
+})
+
+$(function(){
+    $(document).on('click','#ex4-btn',function(){
+        $('#ex4-btn').prop('disabled',true);
+    })
+})
+
+$(function(){
+    $(document).on('click','#ex5-btn',function(){
+        if ($('#ex5-email').val() == "") {
+            alert("メールアドレスを入力してください");
+        }
+    })
+})
